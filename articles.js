7748e30c8cc59a1971f08d152918e4a1463c540a
RobiPlay <156914260+RobiPlay@users.noreply.github.com>
2025-07-18 23:09:39 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -25,15 +25,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const newsData = [
     {
-        title: "Żegnamy Scroll 1",
-        date: "2 kwietnia 2025",
-        content: "Żegnamy Scroll 1 2 kwietnia 2025 Chociaż większość z was nie miała okazji doświadczyć najstarszego systemu, stanowi on fundament dla wszystkich późniejszych kampanii, takich jak The Witcher RPG oraz SKRIEWE. System ten był ograniczony, cechował się liniowością, prostą ścieżką fabularną i sztywnymi scenariuszami. Mimo to posiadał pełne zasady, co stanowiło dużą zaletę w porównaniu do eksperymentalnego charakteru swojego następcy. <br><br>W ramach pożegnania odnowiłem kilka prostych kampanii, które oddają charakter tego wyjątkowego systemu. Szczególnie ważnym tytułem jest 'SKRIEWE: Peace and War', lustrzane odbicie oryginalnej kampanii, które w świetny sposób streszcza całą fabułę. Zastanówcie się nad dodaniem tych tytułów do swojej biblioteki i wypróbowaniem ich w dogodnym czasie.<br><br> Miłej gry! :)"
-
-    },
-    {
         title: "Nadchodzi....",
         date: "18 lipca 2025",
         content: "Po Ravenburg: Asylum, ARAMie i dwóch sezonach AoT nadszedł wreście czas by przeżyć kolejną przygodę. Kiedy planowane na sierpień testy dobiegną końca, ofijalnie pojawi się Scroll III oraz SKRIEWE III, system stworzony do gry czy gra stworzona do systemu? Przygotujcie się też na hordę okazji w sklepie, masę darmówek i nowych tytułów które zapamiętacie na długo. Oczekujcie zapowiedzi SCROLL'a III i szykujcie się na ostrą jazdę (w 4K 240FPS ;D)"
+
+    },
+    {
+        title: "Żegnamy Scroll 1",
+        date: "2 kwietnia 2025",
+        content: "Żegnamy Scroll 1 2 kwietnia 2025 Chociaż większość z was nie miała okazji doświadczyć najstarszego systemu, stanowi on fundament dla wszystkich późniejszych kampanii, takich jak The Witcher RPG oraz SKRIEWE. System ten był ograniczony, cechował się liniowością, prostą ścieżką fabularną i sztywnymi scenariuszami. Mimo to posiadał pełne zasady, co stanowiło dużą zaletę w porównaniu do eksperymentalnego charakteru swojego następcy. <br><br>W ramach pożegnania odnowiłem kilka prostych kampanii, które oddają charakter tego wyjątkowego systemu. Szczególnie ważnym tytułem jest 'SKRIEWE: Peace and War', lustrzane odbicie oryginalnej kampanii, które w świetny sposób streszcza całą fabułę. Zastanówcie się nad dodaniem tych tytułów do swojej biblioteki i wypróbowaniem ich w dogodnym czasie.<br><br> Miłej gry! :)"
 
     }
 ];

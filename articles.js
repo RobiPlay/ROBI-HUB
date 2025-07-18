fe6761a9566dea8f729b858a19cc11977f5b7d34
RobiPlay <156914260+RobiPlay@users.noreply.github.com>
2025-07-18 22:56:39 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -29,7 +29,7 @@ const newsData = [
         date: "2 kwietnia 2025",
         content: "Żegnamy Scroll 1 2 kwietnia 2025 Chociaż większość z was nie miała okazji doświadczyć najstarszego systemu, stanowi on fundament dla wszystkich późniejszych kampanii, takich jak The Witcher RPG oraz SKRIEWE. System ten był ograniczony, cechował się liniowością, prostą ścieżką fabularną i sztywnymi scenariuszami. Mimo to posiadał pełne zasady, co stanowiło dużą zaletę w porównaniu do eksperymentalnego charakteru swojego następcy. <br><br>W ramach pożegnania odnowiłem kilka prostych kampanii, które oddają charakter tego wyjątkowego systemu. Szczególnie ważnym tytułem jest 'SKRIEWE: Peace and War', lustrzane odbicie oryginalnej kampanii, które w świetny sposób streszcza całą fabułę. Zastanówcie się nad dodaniem tych tytułów do swojej biblioteki i wypróbowaniem ich w dogodnym czasie.<br><br> Miłej gry! :)"
 
-    }
+    },
     {
         title: "Nadchodzi....",
         date: "18 lipca 2025",

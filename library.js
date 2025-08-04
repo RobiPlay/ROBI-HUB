a5f3624c999ff273d47de7bb32174a4591a75dbe
RobiPlay <156914260+RobiPlay@users.noreply.github.com>
2025-08-05 01:37:33 +0200

    Update library.js

--- library.js
+++ library.js
@@ -3,16 +3,17 @@ const libraryData = [
         "title": "Attack on Titan - seasons 1-3",
         "system": "Scroll 2",
         "rating": "8.0",
-        "image": "aot.jpg"  // Zastąp prawdziwą ścieżką do obrazu
-        "color": "#28a745",   // zielony
+        "image": "aot.jpg",  // Zastąp prawdziwą ścieżką do obrazu
+        "color": "#28a745"   // zielony
     },
     {
         "title": "Ravenburg Asylum: Definitive Edition",
         "system": "Scroll 1",
         "rating": "8.5",
-        "image": "raven.jpg"  // Zastąp prawdziwą ścieżką do obrazu
-        "color": "#28a745",   // zielony
+        "image": "raven.jpg",  // Zastąp prawdziwą ścieżką do obrazu
+        "color": "#28a745"   // zielony
     }
     // Możesz dodać więcej kampanii
 ];
+
 

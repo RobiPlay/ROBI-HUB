97e6a42ea1f82863e61c011cae96c6b8f9455c3e
RobiPlay <156914260+RobiPlay@users.noreply.github.com>
2025-08-05 01:35:45 +0200

    Update library.js

--- library.js
+++ library.js
@@ -4,12 +4,15 @@ const libraryData = [
         "system": "Scroll 2",
         "rating": "8.0",
         "image": "aot.jpg"  // Zastąp prawdziwą ścieżką do obrazu
+        "color": "#28a745",   // zielony
     },
     {
         "title": "Ravenburg Asylum: Definitive Edition",
         "system": "Scroll 1",
         "rating": "8.5",
         "image": "raven.jpg"  // Zastąp prawdziwą ścieżką do obrazu
+        "color": "#28a745",   // zielony
     }
     // Możesz dodać więcej kampanii
 ];
+
